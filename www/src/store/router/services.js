a4c0fe213bd0a4564f46cc73d2b31804aac2de8a
Razvan Gherghina <Razvan Gherghina>
2017-02-12 12:02:26 +0100

    services an actions

--- www/src/store/router/services.js
+++ www/src/store/router/services.js
@@ -5,18 +5,23 @@ import createHistory from 'history/createBrowserHistory'
 
 const history = createHistory()
 
-// if route is requested history will navigate to it
+// if route is requested history will navigate to it if route if different then current route
+// or will invoke route resolved if is the same route
 payloads$(Actions.ROUTE_REQUESTED)
-  .filter(pathname => pathname && (history.location.pathname !== pathname))
-  .subscribe(history.push)
+  .subscribe(pathname => {
+    if (pathname && (history.location.pathname !== pathname)) {
+      history.push(pathname)
+    } else {
+      Handlers.routeResolved()
+    }
+  })
 
 // all routes pulled from history event are dispatched into router store
 Observable
+  // all changes in history
   .fromEventPattern(history.listen)
+  // starting with current route
   .merge(Observable.of(history.location))
-  // history changes key with the same pathname for many times, so ignoring multiple event call
-  .debounceTime(16)
-  .withLatestFrom(payloads$(Actions.ROUTE_CHANGED).startWith({}), (location, route) => ({location, route}))
-  .filter((location, route) => !route.pathname || (route.pathname !== location.pathname))
-  .pluck('location')
-  .subscribe(Handlers.changeRoute)
+  // only distinct pathnames
+  .distinctUntilChanged((route, oldroute) => route.pathname !== oldroute.pathname)
+  .subscribe(route => Handlers.changeRoute(route))
