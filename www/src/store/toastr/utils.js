74cd6629e0aa5c73dfdab3adf9dd25b365f604f6
Razvan Gherghina <gherghina.razvan@gmail.com>
2017-02-14 21:49:11 +0100

    recycle toastr id for same message

--- www/src/store/toastr/utils.js
+++
@@ -1,5 +0,0 @@
-
-export function guid () {
-  const r = () => Math.floor((1 + Math.random()) * 0x10000).toString(16).substring(1)
-  return r() + r() + r() + '-' + r() + '_' + r() + '-' + r() + '_' + r() + r() + r()
-}
