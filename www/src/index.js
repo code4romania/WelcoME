f9ef2f8c1fc3b2d75437e645522fd29400409761
Razvan Gherghina <gherghina.razvan@gmail.com>
2017-02-26 20:02:08 +0100

    header

--- www/src/index.js
+++ www/src/index.js
@@ -1,15 +1,17 @@
 import React from 'react'
 import { render } from 'react-dom'
-
+import injectTapEventPlugin from 'react-tap-event-plugin'
 import { store$, handlers } from './store'
 import Provider from './rxdux/Provider'
 import App from './components/App'
 import './index.css'
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
+injectTapEventPlugin()
 
 // inject state and handlers into context of React components
 store$.subscribe(store => render(
   <Provider store={store} handlers={handlers}>
-    <App />
+    <MuiThemeProvider><App /></MuiThemeProvider>
   </Provider>,
   document.getElementById('app')
 ))
