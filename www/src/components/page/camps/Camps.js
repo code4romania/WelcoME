a99e1ef8929fc6a00e308fb5528a09fdc652ab31
Razvan Gherghina <gherghina.razvan@gmail.com>
2017-03-05 00:05:05 +0100

    roles and camps

--- www/src/components/page/camps/Camps.js
+++ www/src/components/page/camps/Camps.js
@@ -16,39 +16,43 @@ const style = {
 }
 
 const Camps = (props, {store, handlers}) => {
-  const owner = store.auth.profile && store.auth.profile.owner
   const { selectedCamp } = store.camps
   const camps = Object.keys(store.camps.camps).map(cid => ({ cid, ...store.camps.camps[cid] }))
-  const users = []
+  const users = Object.keys(store.camps.userList).map(uid => ({ uid, ...store.camps.users[uid] }))
   return (
     <Card >
       <CardActions>
-        { owner
-        ? <FloatingActionButton onClick={() => handlers.addEmptyCamp()} mini style={style} title='Add camp'>
+        <FloatingActionButton onClick={() => handlers.addEmptyCamp()} mini style={style} title='Add camp'>
           <ContentAdd />
-        </FloatingActionButton> : null}
+        </FloatingActionButton>
       </CardActions>
       <CardText>
         <List>
-          {camps.map(camp => <Camp key={camp.cid} activeCamp={selectedCamp === camp.cid ? 'activeCamp' : ''}
+          {camps.map(camp => <Camp key={camp.cid} activeCamp={selectedCamp === camp.cid}
             onDeleteCamp={() => handlers.removeCamp(camp.cid)}
             onSelectCamp={() => {
               handlers.changeFields({
                 cid: camp.cid,
-                name: camp.name,
-                somekey: camp.somekey
+                name: camp.name
               })
               handlers.selectCamp(camp.cid)
             }}
-            hasRemove={!!owner && (camp.cid !== 'default')} {...camp} />)}
+            onDeselectCamp={() => {
+              handlers.clearFields()
+              handlers.selectCamp()
+            }}
+            hasRemove={camp.cid !== 'default'} {...camp} />)}
         </List>
-        {selectedCamp ? <div>
+        {selectedCamp && users.length ? <div>
           <Divider />
           <List>
-            {users.map(user => <User {...user} />)}
+            {users.map(user => <User key={user.uid} {...user}
+              onAdmin={() => handlers.makeAdmin(user.uid, selectedCamp)}
+              offAdmin={() => handlers.removeAdmin(user.uid, selectedCamp)}
+            />)}
           </List>
         </div> : null }
-        {selectedCamp && owner
+        {selectedCamp
         ? <div><Divider />
           <CampForm />
         </div>
--- www/src/store/camps/reducer.js
+++ www/src/store/camps/reducer.js
@@ -7,6 +7,10 @@ registerAction('ADD_EMPTY_CAMP')
 registerAction('REMOVE_CAMP')
 registerAction('UPDATE_CAMP')
 registerAction('SELECT_CAMP')
+registerAction('USERS_CHANGE')
+registerAction('ONEUSER_CHANGE')
+registerAction('MAKE_ADMIN')
+registerAction('REMOVE_ADMIN')
 
 // handlers
 Handlers.addEmptyCamp = camp => dispatch(Actions.ADD_EMPTY_CAMP, camp)
@@ -14,10 +18,16 @@ Handlers.removeCamp = cid => dispatch(Actions.REMOVE_CAMP, cid)
 Handlers.updateCamp = camp => dispatch(Actions.UPDATE_CAMP, camp)
 Handlers.selectCamp = cid => dispatch(Actions.SELECT_CAMP, cid)
 Handlers.campsChanged = camps => dispatch(Actions.CAMPS_CHANGED, camps)
+Handlers.usersChanged = uids => dispatch(Actions.USERS_CHANGE, uids)
+Handlers.userChange = (uid, volunteer) => dispatch(Actions.ONEUSER_CHANGE, { [uid]: volunteer })
 
+Handlers.makeAdmin = (uid, cid) => dispatch(Actions.MAKE_ADMIN, {uid, cid})
+Handlers.removeAdmin = (uid, cid) => dispatch(Actions.REMOVE_ADMIN, {uid, cid})
 // reducer
 const initialState = {
   camps: {},
+  userList: [],
+  users: {},
   selectedCamp: null
 }
 
@@ -26,6 +36,8 @@ Reducers.camps = (state = initialState, action) => {
     case Actions.ADD_EMPTY_CAMP:
     case Actions.REMOVE_CAMP:
     case Actions.UPDATE_CAMP:
+    case Actions.MAKE_ADMIN:
+    case Actions.REMOVE_ADMIN:
       return state
     case Actions.SELECT_CAMP:
       return {
@@ -37,6 +49,17 @@ Reducers.camps = (state = initialState, action) => {
         ...state,
         camps: action.payload || {}
       }
+    case Actions.USERS_CHANGE:
+      return {
+        ...state,
+        userList: action.payload ? {...state.userList, ...action.payload} : {}
+      }
+    case Actions.ONEUSER_CHANGE:
+      return {
+        ...state,
+        users: {...state.users, ...action.payload}
+      }
+
     default:
       return state
   }
