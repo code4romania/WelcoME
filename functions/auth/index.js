8331269ce51de8442ed61137ed0581fafef2307b
Razvan Gherghina <gherghina.razvan@gmail.com>
2017-04-19 18:55:31 +0200

    function oauth2 email send

--- functions/auth/index.js
+++ functions/auth/index.js
@@ -1,7 +1,41 @@
+'use strict'
+const functions = require('firebase-functions')
 const admin = require('firebase-admin')
+const nodemailer = require('nodemailer')
+const rs = require('randomstring')
+const gmailEmail = functions.config().gmail.email
+const gmailPassword = functions.config().gmail.password
+const mailTransport = nodemailer.createTransport({
+  service: 'Gmail',
+  auth: {
+    type: 'OAuth2',
+    user: gmailEmail,
+    clientId: '578400874611-h8lfd7t3g8prfjrvba36m5srabcjqffi.apps.googleusercontent.com',
+    clientSecret: gmailPassword,
+    refreshToken: '1/U3veQsdoGsq0-1YTAij6eEhCG2PQwlrLObYGAna3nDg'
+  }
+}, {
+  from: gmailEmail
+})
 
 const sendVerificationEmail = ({email, lang}) => {
-  console.log('Send verification email', email, lang)
+  console.log('Send2 verification email', mailTransport, email, lang)
+  const mailOptions = {
+    to: email
+  }
+  const code = rs.generate(20)
+  mailOptions.subject = 'Verify email for WelcoME!'
+  mailOptions.text = `   Hello ${email},
+        Follow this link to verify your email address.
+        http://localhost:3000/actions?mode=verifyEmail&oobCode=${code}&email=${email}
+        If you didn’t ask to verify this address, you can ignore this email.
+        Thanks,
+        Your Welcome team
+  `
+  return mailTransport.sendMail(mailOptions).then(() => {
+    console.log('New subscription confirmation email sent to:', email)
+    mailTransport.close()
+  }).catch(err => console.warn('Error sending mail', err))
 }
 
 // when account changes update users key
@@ -16,8 +50,8 @@ const profileModified = event => {
   const uid = event.params.uid
   const val = event.data.val()
 
-  // when key is cancelled do nothing
-  if (!val) {
+  // when key is cancelled or empty do nothing
+  if (!val || !Object.keys(val).length) {
     return
   }
 
@@ -38,7 +72,8 @@ const profileModified = event => {
 
     // if send verification email
     if (!emailVerified && val.sendVerificationEmail) {
-      return sendVerificationEmail({ email: user.email, lang: val.lang })
+      sendVerificationEmail({ email: user.email, lang: val.lang })
+      return admin.database().ref(`/usersWrites/${uid}`).remove()
     }
 
     // accept only some keys in users profile
@@ -64,7 +99,7 @@ const profileModified = event => {
     upd[`/users/${uid}`] = val
     upd[`/usersWrites/${uid}`] = null
     admin.database().ref().update(upd)
-  })
+  }).catch(err => console.warn('User not found', err))
 }
 
 // when user is created update users key
