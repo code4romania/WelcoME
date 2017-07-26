d27cbd6bc230cb02caa90fc0d37980cd347d719e
Mihai <mihaiveronica@ymail.com>
2017-07-26 15:47:46 +0300

    removed LocationStep and SkillStep rendering through ProfileCreationStep

--- www/src/components/pages/profile_creation/ProfileCreationStep.js
+++ www/src/components/pages/profile_creation/ProfileCreationStep.js
@@ -1,28 +1,12 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import LocationStep from './LocationStep'
-import SkillsStep from './SkillsStep'
 
-
-const ProfileCreationStep = ({ text , onChangeKey, campLocation, campCountry, camps, countries, studies, skills }) => {
-    if (text === "location") {
-      return ( 
-      	 <LocationStep  {...{onChangeKey, campLocation, campCountry, camps,countries}} />
-      	 );
-    } else if (text === "skills") {
-      return ( 
-      	 <SkillsStep  {...{studies,skills}} />
-      	 );
-    }
-
-    else {
+const ProfileCreationStep = ({ text}) => {
         return ( 
         < div > 
         	{ text } 
         < /div>
         );
-    }
-
 }
 
 ProfileCreationStep.PropTypes = {
